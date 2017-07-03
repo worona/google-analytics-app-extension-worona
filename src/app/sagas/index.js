52e46d15a6f449ce65f2aceb48d9bd68ec5de644
David Arenas <darensr@gmail.com>
2017-07-03 16:30:40 +0200

    Improvement of function virtualPageView. Switch case fixed.

--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -7,29 +7,35 @@ import * as deps from '../deps';
 export function* virtualPageView(siteName, siteUrl) {
   const query = yield select(deps.selectors.getURLQueries);
 
+  const wpTypeSucceed = (actionType, id) => action =>
+    action.type === actionType && action.id === id;
+
+  const currentListSucceed = () => action =>
+    action.type === deps.types.NEW_POSTS_LIST_SUCCEED && action.name === 'currentList';
+
   if (query.p) {
-    yield take(deps.types.POST_SUCCEED);
+    yield take(wpTypeSucceed(deps.types.POST_SUCCEED, query.p));
     yield call(sendPageView, siteName, siteUrl, 'posts', query.p);
   } else if (query.cat) {
-    yield take(deps.types.NEW_POSTS_LIST_SUCCEED);
+    yield take(currentListSucceed());
     yield call(sendPageView, siteName, siteUrl, 'categories', query.cat);
   } else if (query.tag) {
-    yield take(deps.types.NEW_POSTS_LIST_SUCCEED);
+    yield take(currentListSucceed());
     yield call(sendPageView, siteName, siteUrl, 'tags', query.tag);
   } else if (query.author) {
-    yield take(deps.types.NEW_POSTS_LIST_SUCCEED);
+    yield take(currentListSucceed());
     yield call(sendPageView, siteName, siteUrl, 'users', query.author);
   } else if (query.page_id) {
-    yield take(deps.types.PAGE_SUCCEED);
+    yield take(wpTypeSucceed(deps.types.PAGE_SUCCEED, query.page_id));
     yield call(sendPageView, siteName, siteUrl, 'pages', query.page_id);
   } else if (query.s) {
-    yield take(deps.types.NEW_POSTS_LIST_SUCCEED);
+    yield take(currentListSucceed());
     yield call(sendPageView, siteName, siteUrl, 'searchs', query.s);
   } else if (query.attachment_id) {
-    yield take(deps.types.POST_SUCCEED);
+    yield take(wpTypeSucceed(deps.types.POST_SUCCEED, query.attachment_id));
     yield call(sendPageView, siteName, siteUrl, 'media', query.attachment_id);
   } else {
-    yield take(deps.types.NEW_POSTS_LIST_SUCCEED);
+    yield take(currentListSucceed());
     yield call(sendPageView, siteName, siteUrl);
   }
 }
@@ -38,18 +44,21 @@ export function* sendPageView(siteName, siteUrl, wpType, id) {
   let entity,
     title;
 
-  console.log('PAGE VIEW', wpType, id);
-
   if (typeof wpType === 'string' && typeof id === 'string') {
     entity = yield select(deps.selectorCreators.getWpTypeById(wpType, id));
   }
 
   // Chooses the correct attribute for pageview's title
   switch (wpType) {
-    case ('posts', 'pages', 'searchs'):
+    case 'posts':
+    case 'pages':
+    case 'searchs':
       title = `${entity.title.rendered} - ${siteName}`;
       break;
-    case ('categories', 'tags', 'users', 'media'):
+    case 'categories':
+    case 'tags':
+    case 'users':
+    case 'media':
       title = `${entity.name} - ${siteName}`;
       break;
     default:
@@ -57,7 +66,6 @@ export function* sendPageView(siteName, siteUrl, wpType, id) {
   }
 
   const entityUrl = entity ? new URL(entity.link) : new URL(siteUrl);
-
   const pageview = {
     hitType: 'pageview',
     title,
@@ -65,21 +73,22 @@ export function* sendPageView(siteName, siteUrl, wpType, id) {
   };
 
   ga('clientTracker.send', pageview);
-
-  console.log(pageview);
 }
 
 export default function* googleAnalyticsSagas() {
-  // yield take(deps.types.SITE_ID_CHANGED);
-
   if (!window.ga) {
-    console.log('Variable "ga" not initialized.');
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  } else {
-    console.log('Variable "ga" already initialized.');
+    (function (i, s, o, g, r, a, m) {
+      i.GoogleAnalyticsObject = r;
+      (i[r] =
+        i[r] ||
+        function () {
+          (i[r].q = i[r].q || []).push(arguments);
+        }), (i[r].l = 1 * new Date());
+      (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m);
+    }(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga'));
   }
 
   const siteUrl = yield select(deps.selectorCreators.getSetting('generalSite', 'url'));
@@ -91,7 +100,6 @@ export default function* googleAnalyticsSagas() {
       deps.selectorCreators.getSetting('googleAnalytics', 'trackingId')
     );
     ga('create', trackingId, 'auto', 'clientTracker');
-    console.log('Client Tracker created.', ga.getAll());
 
     yield call(virtualPageView, siteName, siteUrl);
   });
